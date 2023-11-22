f88034e034c365eddbc21d42426ec98be27b992d
qinqiang <13186061107@163.com>
2023-11-22 17:27:06 +0800

    输入输出封装、使用JDK动态代理

---
+++ src/main/java/lxqq/rpc/version2/client/IOClient.java
@@ -0,0 +1,46 @@
+package lxqq.rpc.version2.client;
+
+import lxqq.rpc.version2.entity.RPCRequest;
+import lxqq.rpc.version2.entity.RPCResponse;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+/**
+ * IOClient类用于进行客户端的输入输出操作
+ */
+public class IOClient {
+    /**
+     * send方法用于向指定的主机和端口发送RPC请求并获取响应数据
+     * @param host 主机名
+     * @param port 端口号
+     * @param request RPC请求对象
+     * @return 响应数据
+     */
+    public static Object send(String host, int port, RPCRequest request) {
+        try {
+            // 创建Socket对象并连接到指定的主机和端口
+            Socket s = new Socket(host,port);
+            // 创建ObjectOutputStream对象用于向输出流发送对象
+            ObjectOutputStream oos = new ObjectOutputStream(s.getOutputStream());
+            // 创建ObjectInputStream对象用于从输入流接收对象
+            ObjectInputStream ois = new ObjectInputStream(s.getInputStream());
+            // 将RPC请求对象序列化并发送给服务器端
+            oos.writeObject(request);
+            oos.flush();
+            // 从服务器端接收响应对象，并将其转换为RPCResponse类型
+            RPCResponse response = (RPCResponse) ois.readObject();
+            // 打印服务器端返回的消息
+            System.out.println("接受到服务端消息："+ response);
+            // 返回响应数据
+            return response.getData();
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        // 如果发生异常，则返回null
+        return null;
+    }
+}
+
---
+++ src/main/java/lxqq/rpc/version2/proxy/ClientProxy.java
@@ -0,0 +1,43 @@
+package lxqq.rpc.version2.proxy;
+
+import lombok.AllArgsConstructor;
+import lxqq.rpc.version2.client.IOClient;
+import lxqq.rpc.version2.entity.RPCRequest;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+@AllArgsConstructor
+public class ClientProxy implements InvocationHandler {
+    // 主机地址
+    private String host;
+    // 端口号
+    private int port;
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) {
+        // 构建 RPC 请求
+        RPCRequest request = RPCRequest.builder()
+                .interfaceName(method.getDeclaringClass().getName())
+                .methodName(method.getName())
+                .params(args)
+                .paramsTypes(method.getParameterTypes())
+                .build();
+        // 调用 IOClient 发送请求并返回结果
+        return IOClient.send(host, port, request);
+    }
+
+    /**
+     * 创建代理对象
+     *
+     * @param clazz 代理对象类型
+     * @return 代理对象
+     */
+    public <T>T getProxy(Class<T> clazz){
+        // 使用 Proxy.newProxyInstance 创建代理对象
+        Object o = Proxy.newProxyInstance(clazz.getClassLoader(), new Class[]{clazz}, this);
+        return (T)o;
+    }
+}
+
